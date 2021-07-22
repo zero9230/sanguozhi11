4be6a8e1b081729c0462d89d3ffa2502add6925e
sikou <nianyang.mny@alibaba-inc.com>
2021-07-22 11:26:30 +0800

    init

--- sanguozhi11-core/src/main/java/nianyang/mny/leetcode/P50.java
+++ sanguozhi11-core/src/main/java/nianyang/mny/leetcode/P50.java
@@ -1,5 +1,8 @@
 package nianyang.mny.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author sikou
  * @date 2021/07/21
@@ -19,11 +22,33 @@ public class P50 {
         if(x==0 || x ==1){
             return x;
         }
-        //类似二分法
-        double res=0.0;
+
+        double res=1;
         //2^37  2^32 , 2^4,2^1
         //把n拆分成2的多项式表达
+        //用一个list装载2的多项式的次数，从2^0,2^1,...,2^x
 
-        return 0;
+        //list中的元素指数从低到高
+        List<Double> list=new ArrayList<>();
+
+        String nBin=Integer.toBinaryString(n);
+        //求得n的二项式的和
+        // x^27 = x^(16+8+2+1)
+
+        double base=x;
+        for(int i=nBin.length()-1;i>=0;i--){
+            double element = base * base;
+            int bitFlag=nBin.charAt(i)-'0';
+            if(bitFlag==1){
+                list.add(element);
+            }
+        }
+        for(Double ele:list){
+            res*=ele;
+        }
+
+        return res;
+
     }
+
 }
