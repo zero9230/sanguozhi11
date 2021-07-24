714a0611a77473dbe4ea18b3ae388e2e6388367a
sikou <nianyang.mny@alibaba-inc.com>
2021-07-24 17:26:22 +0800

    init

---
+++ sanguozhi11-core/src/main/java/nianyang/mny/interview/demo/BaiduDemo3.java
@@ -0,0 +1,11 @@
+package nianyang.mny.interview.demo;
+
+/**
+ * @author sikou
+ * @date 2021/07/23
+ */
+public class BaiduDemo3 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
