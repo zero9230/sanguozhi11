85460fcfe48123562bbc6e1b4f7d0f686e9e52c5
zero9230 <mengnianyang9230@163.com>
2021-10-27 11:28:07 +0800

    n

--- sanguozhi11-core/src/main/java/nianyang/mny/test/lru/MyLRU.java
+++ sanguozhi11-core/src/main/java/nianyang/mny/test/lru/MyLRU.java
@@ -1,2 +1,14 @@
-package nianyang.mny.test.lru;public class MyLru {
+package nianyang.mny.test.lru;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MyLRU<K,V> extends LinkedHashMap<K,V> {
+
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K,V> eldest) {
+        return false;
+    }
+
 }
